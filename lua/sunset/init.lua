15990b58920aba6a4627b73253995ef88d978f87
Joshua Manchester <joshuamanchester4@gmail.com>
2022-09-21 21:48:47 +0100

    Fixed formatting

--- lua/sunset/init.lua
+++ lua/sunset/init.lua
@@ -8,171 +8,177 @@ local is_day_forced = nil -- user's manually set day/night state
 local next_sunrise = nil
 local next_sunset = nil
 local timer = nil
-
-local default_opts = {
-	latitude = 51.5072, -- default to London
-	longitude = -0.1276,
-	sunrise_offset = 0, -- seconds
-	sunset_offset = 0, -- seconds
-	sunrise_override = nil,
-	sunset_override = nil,
-	day_callback = nil,
-	night_callback = nil,
-	update_interval = 60000, -- milliseconds
-}
-
 local opts = {}
 
+local default_opts = {
+    latitude = 51.5072, -- north is positive, south is negative
+    longitude = -0.1276, -- east is positive, west is negative
+    sunrise_offset = 0, -- offset the sunrise by this many seconds
+    sunset_offset = 0, -- offset the sunset by this many seconds
+    sunrise_override = nil, -- accepts a time in the form "HH:MM" which will override the sunrise time
+    sunset_override = nil, -- accepts a time in the form "HH:MM" which will override the sunrise time
+    day_callback = nil, -- function that is called when day begins
+    night_callback = nil, -- function that is called when night begins
+    update_interval = 60000, -- how frequently to check for sunrise/sunset changes in milliseconds
+}
+
 local trigger_night = function()
-	is_day_forced = false
-	if opts.night_callback then
-		opts.night_callback()
-	else
-		util.set_background("dark")
-	end
+    is_day_forced = false
+    if opts.night_callback then
+        opts.night_callback()
+    else
+        util.set_background("dark")
+    end
 end
 
 local trigger_day = function()
-	is_day_forced = true
-	if opts.day_callback then
-		opts.day_callback()
-	else
-		util.set_background("light")
-	end
+    is_day_forced = true
+    if opts.day_callback then
+        opts.day_callback()
+    else
+        util.set_background("light")
+    end
 end
 
 --- Updates the next_sunrise and next_sunset values
 local update_sun_times = function()
-	-- if both are overriden no need to calculate next sun times
-	if opts.sunrise_override and opts.sunset_override then
-		next_sunrise = util.str_to_next_time(opts.sunrise_override)
-		next_sunset = util.str_to_next_time(opts.sunset_override)
-		return
-	end
+    -- if both are overriden no need to calculate next sun times
+    if opts.sunrise_override and opts.sunset_override then
+        next_sunrise = util.str_to_next_time(opts.sunrise_override)
+        next_sunset = util.str_to_next_time(opts.sunset_override)
+        return
+    end
 
-	local sun_times = util.next_sun_times(opts.latitude, opts.longitude, opts.sunrise_offset, opts.sunset_offset)
-	next_sunrise = util.str_to_next_time(opts.sunrise_override) or sun_times.sunrise
-	next_sunset = util.str_to_next_time(opts.sunset_override) or sun_times.sunset
+    local sun_times = util.next_sun_times(opts.latitude, opts.longitude, opts.sunrise_offset, opts.sunset_offset)
+    next_sunrise = util.str_to_next_time(opts.sunrise_override) or sun_times.sunrise
+    next_sunset = util.str_to_next_time(opts.sunset_override) or sun_times.sunset
 end
 
 --- Checks if sunrise/sunset is outdated and triggers day/night transition if
 --- the sun's state has changed.
 local update_theme = function()
-	if next_sunrise == nil or next_sunset == nil then
-		notify.error("Plugin stopping due to sunrise/sunset error.")
-		if timer ~= nil then
-			timer:stop()
-		end
-		return
-	end
+    if next_sunrise == nil or next_sunset == nil then
+        notify.error("Plugin stopping due to sunrise/sunset error.")
+        if timer ~= nil then
+            timer:stop()
+        end
+        return
+    end
 
-	local time = os.time()
-	-- update sunrise/sunset time if either is outdated
-	if time > next_sunrise or time > next_sunset then
-		update_sun_times()
-	end
+    local time = os.time()
+    -- update sunrise/sunset time if either is outdated
+    if time > next_sunrise or time > next_sunset then
+        update_sun_times()
+    end
 
-	-- use the next sunset and sunrise times to determine if the sun is up
-	if is_day and next_sunrise < next_sunset then
-		is_day = false
-		trigger_night()
-	elseif not is_day and next_sunset < next_sunrise then
-		is_day = true
-		trigger_day()
-	end
+    -- use the next sunset and sunrise times to determine if the sun is up
+    if is_day and next_sunrise < next_sunset then
+        is_day = false
+        trigger_night()
+    elseif not is_day and next_sunset < next_sunrise then
+        is_day = true
+        trigger_day()
+    end
 end
 
 --- Validates and loads options. Resorts to default values if there's an error
 --- with the user's options.
+---@param new_opts table
 local load_opts = function(new_opts)
-	if not new_opts then
-		new_opts = {}
-	end
+    if not new_opts then
+        new_opts = {}
+    end
 
-	if new_opts.latitude and (new_opts.latitude < -90 or new_opts.latitude > 90) then
-		notify.warn("Invalid latitude value. Must be between -90 and 90 inclusive.")
-		new_opts.latitude = default_opts.latitude
-	end
+    if new_opts.latitude and (new_opts.latitude < -90 or new_opts.latitude > 90) then
+        notify.warn("Invalid latitude value. Must be between -90 and 90 inclusive.")
+        new_opts.latitude = default_opts.latitude
+    end
 
-	if new_opts.longitude and (new_opts.longitude < -180 or new_opts.longitude > 180) then
-		notify.warn("Invalid longitude value. Must be between -180 and 180 inclusive.")
-		new_opts.longitude = default_opts.longitude
-	end
+    if new_opts.longitude and (new_opts.longitude < -180 or new_opts.longitude > 180) then
+        notify.warn("Invalid longitude value. Must be between -180 and 180 inclusive.")
+        new_opts.longitude = default_opts.longitude
+    end
 
-	if new_opts.update_interval and (new_opts.update_interval <= 0) then
-		notify.warn("Invalid update interval milliseconds. Must be greater than 0.")
-		new_opts.update_interval = default_opts.update_interval
-	end
+    if new_opts.update_interval and (new_opts.update_interval <= 0) then
+        notify.warn("Invalid update interval milliseconds. Must be greater than 0.")
+        new_opts.update_interval = default_opts.update_interval
+    end
 
-	if new_opts.sunrise_offset then
-		new_opts.sunrise_offset = new_opts.sunrise_offset % (86400 * util.sign(new_opts.sunrise_offset))
-	end
+    if new_opts.sunrise_offset then
+        new_opts.sunrise_offset = new_opts.sunrise_offset % (86400 * util.sign(new_opts.sunrise_offset))
+    end
 
-	if new_opts.sunset_offset then
-		new_opts.sunset_offset = new_opts.sunset_offset % (86400 * util.sign(new_opts.sunset_offset))
-	end
+    if new_opts.sunset_offset then
+        new_opts.sunset_offset = new_opts.sunset_offset % (86400 * util.sign(new_opts.sunset_offset))
+    end
 
-	opts = vim.tbl_extend("force", default_opts, new_opts)
+    opts = vim.tbl_extend("force", default_opts, new_opts)
 end
 
+--- Prints the next sunrise and sunset times
 local print_sun_times = function()
-	if next_sunrise == nil or next_sunset == nil then
-		notify.error("Cannot print sun times due to error.")
-	end
+    if next_sunrise == nil or next_sunset == nil then
+        notify.error("Cannot print sun times due to error.")
+    end
 
-	local sunrise_message = ""
-	if opts.sunrise_override then
-		sunrise_message = " overriden to"
-	elseif opts.sunrise_offset ~= 0 then
-		sunrise_message = " offset to"
-	end
+    local sunrise_message = ""
+    if opts.sunrise_override then
+        sunrise_message = " overriden to"
+    elseif opts.sunrise_offset ~= 0 then
+        sunrise_message = " offset to"
+    end
 
-	local sunset_message = ""
-	if opts.sunset_override then
-		sunset_message = " overriden to"
-	elseif opts.sunset_offset ~= 0 then
-		sunset_message = " offset to"
-	end
+    local sunset_message = ""
+    if opts.sunset_override then
+        sunset_message = " overriden to"
+    elseif opts.sunset_offset ~= 0 then
+        sunset_message = " offset to"
+    end
 
-	local message = "Next sunrise is%s: %s\nNext sunset is%s: %s"
-	message = message:format(sunrise_message, os.date("%c", next_sunrise), sunset_message, os.date("%c", next_sunset))
-	notify.info(message)
+    local message = "Next sunrise is%s: %s\nNext sunset is%s: %s"
+    message = message:format(sunrise_message, os.date("%c", next_sunrise), sunset_message, os.date("%c", next_sunset))
+    notify.info(message)
 end
 
 local commands = {
-	SunsetToggle = function()
-		if is_day_forced then
-			trigger_night()
-		else
-			trigger_day()
-		end
-	end,
-	SunsetTriggerDay = trigger_day,
-	SunsetTriggerNight = trigger_night,
-	SunsetTimes = print_sun_times,
+    SunsetToggle = function()
+        if is_day_forced then
+            trigger_night()
+        else
+            trigger_day()
+        end
+    end,
+    SunsetDay = trigger_day,
+    SunsetNight = trigger_night,
+    SunsetTimes = print_sun_times,
 }
 
+--- Loads the plugin
+---@param new_opts table
 M.setup = function(new_opts)
-	if vim.g.loaded_sunrise == 1 then
-		return
-	end
-	vim.g.loaded_sunrise = 1
+    if vim.g.loaded_sunrise == 1 then
+        return
+    end
+    vim.g.loaded_sunrise = 1
 
-	load_opts(new_opts)
-	update_sun_times()
+    load_opts(new_opts)
+    update_sun_times()
+    if next_sunrise == nil or next_sunset == nil then
+        return
+    end
 
-	-- create commands
-	for command, func in pairs(commands) do
-		vim.api.nvim_create_user_command(command, func, {})
-	end
+    -- create commands
+    for command, func in pairs(commands) do
+        vim.api.nvim_create_user_command(command, func, {})
+    end
 
-	-- set initial is_day value as opposite to trigger the first day/night switch
-	is_day = not (next_sunset < next_sunrise)
-	is_day_forced = is_day
+    -- set initial is_day value as opposite to trigger the first day/night switch
+    is_day = not (next_sunset < next_sunrise)
+    is_day_forced = is_day
 
-	-- start the update_theme timer
-	timer = vim.loop.new_timer()
-	timer:start(0, opts.update_interval, vim.schedule_wrap(update_theme))
+    -- start the update_theme timer
+    timer = vim.loop.new_timer()
+    timer:start(0, opts.update_interval, vim.schedule_wrap(update_theme))
 end
 
 return M
--- lua/sunset/util.lua
+++ lua/sunset/util.lua
@@ -2,36 +2,104 @@ local M = {}
 
 local api = vim.api
 
--- trig functions in degrees
+--- Trig functions in degrees
 local sin = function(x)
-	return math.sin(math.rad(x))
+    return math.sin(math.rad(x))
 end
 local cos = function(x)
-	return math.cos(math.rad(x))
+    return math.cos(math.rad(x))
 end
 local asin = function(x)
-	return math.deg(math.asin(x))
+    return math.deg(math.asin(x))
 end
 local acos = function(x)
-	return math.deg(math.acos(x))
+    return math.deg(math.acos(x))
 end
 
 M.sign = function(x)
-	if x > 0 then
-		return 1
-	elseif x < 0 then
-		return -1
-	else
-		return 0
-	end
+    if x > 0 then
+        return 1
+    elseif x < 0 then
+        return -1
+    else
+        return 0
+    end
 end
 
 local round_to_zero = function(x)
-	if x >= 0 then
-		return math.floor(x)
-	else
-		return math.ceil(x)
-	end
+    if x >= 0 then
+        return math.floor(x)
+    else
+        return math.ceil(x)
+    end
+end
+
+M.set_background = function(background)
+    if api.nvim_get_option("background") ~= background then
+        api.nvim_set_option("background", background)
+    end
+end
+
+--- Notify with plugin title
+---@param message string
+---@param level number
+local notify_func = function(message, level)
+    local exists, notify = pcall(require, "notify")
+    if exists then
+        notify(message, level, { title = "Sunset.nvim", timeout = 20000 })
+    else
+        message = "Sunset.nvim\n" .. message
+        vim.notify(message, level)
+    end
+end
+
+M.notify = {
+    debug = function(message)
+        notify_func(message, vim.log.levels.DEBUG)
+    end,
+    error = function(message)
+        notify_func(message, vim.log.levels.ERROR)
+    end,
+    info = function(message)
+        notify_func(message, vim.log.levels.INFO)
+    end,
+    warn = function(message)
+        notify_func(message, vim.log.levels.WARN)
+    end,
+}
+
+--- Converts a string representing a 24 hour time in the form HH:MM to the next
+--- occurence of this time as a system datetime value. Returns nil if the
+--- provided string is not a valid time.
+---@param time_str string
+---@return number|nil
+M.str_to_next_time = function(time_str)
+    if not time_str or time_str == "" then
+        return nil
+    end
+
+    -- parse string
+    local hour, min = string.match(time_str, "(%d?%d):(%d%d)")
+    if not hour or not min then
+        M.notify.error("Invalid time format provided. Supported format is HH:MM.")
+        return nil
+    elseif tonumber(hour) > 24 or tonumber(min) > 59 then
+        M.notify.error("Invalid time provided. Must be a 24-hour time.")
+        return nil
+    end
+
+    local date = os.date("!*t")
+    local current_time = os.time(date)
+    date.hour = tonumber(hour)
+    date.min = tonumber(min)
+    date.sec = 0
+
+    local next_time = os.time(date)
+    if next_time < current_time then
+        date.day = date.day + 1
+        next_time = os.time(date)
+    end
+    return next_time
 end
 
 --- Converts an osdate to a Julian date
@@ -42,13 +110,13 @@ end
 ---@param date osdate
 ---@return number
 local date_to_julian = function(date)
-	return round_to_zero((1461 * (date.year + 4800 + round_to_zero((date.month - 14) / 12))) / 4)
-		+ round_to_zero((367 * (date.month - 2 - 12 * round_to_zero((date.month - 14) / 12))) / 12)
-		- round_to_zero((3 * round_to_zero((date.year + 4900 + round_to_zero((date.month - 14) / 12)) / 100)) / 4)
-		+ (date.day - 32075)
-		+ ((date.hour - 12) / 24)
-		+ (date.min / 1440)
-		+ (date.sec / 86400)
+    return round_to_zero((1461 * (date.year + 4800 + round_to_zero((date.month - 14) / 12))) / 4)
+        + round_to_zero((367 * (date.month - 2 - 12 * round_to_zero((date.month - 14) / 12))) / 12)
+        - round_to_zero((3 * round_to_zero((date.year + 4900 + round_to_zero((date.month - 14) / 12)) / 100)) / 4)
+        + (date.day - 32075)
+        + ((date.hour - 12) / 24)
+        + (date.min / 1440)
+        + (date.sec / 86400)
 end
 
 --- Converts a Julian date to an osdate
@@ -59,28 +127,28 @@ end
 ---@param julian_date number
 ---@return osdate
 local julian_to_greg = function(julian_date)
-	local date = {}
-	-- date component
-	local julian_day = math.floor(julian_date)
-	local f = julian_day + 1401 + math.floor((math.floor(((4 * julian_day) + 274277) / 146097) * 3) / 4) - 38
-	local e = (4 * f) + 3
-	local g = math.floor((e % 1461) / 4)
-	local h = (5 * g) + 2
-	date.day = math.floor((h % 153) / 5) + 1
-	date.month = ((math.floor(h / 152) + 2) % 12) + 1
-	date.year = math.floor(e / 1461) - 4716 + math.floor((14 - date.month) / 12)
+    local date = {}
+    -- date component
+    local julian_day = math.floor(julian_date)
+    local f = julian_day + 1401 + math.floor((math.floor(((4 * julian_day) + 274277) / 146097) * 3) / 4) - 38
+    local e = (4 * f) + 3
+    local g = math.floor((e % 1461) / 4)
+    local h = (5 * g) + 2
+    date.day = math.floor((h % 153) / 5) + 1
+    date.month = ((math.floor(h / 152) + 2) % 12) + 1
+    date.year = math.floor(e / 1461) - 4716 + math.floor((14 - date.month) / 12)
 
-	-- time component
-	local julian_time = julian_date - julian_day
-	f = 86400 * julian_time
-	e = f % 3600
-	g = math.floor(f / 3600) + 12
-	date.day = date.day + math.floor(g / 24)
-	date.hour = g % 24
-	date.min = math.floor(e / 60)
-	date.sec = math.floor(e % 60)
-	date.isdst = false
-	return date
+    -- time component
+    local julian_time = julian_date - julian_day
+    f = 86400 * julian_time
+    e = f % 3600
+    g = math.floor(f / 3600) + 12
+    date.day = date.day + math.floor(g / 24)
+    date.hour = g % 24
+    date.min = math.floor(e / 60)
+    date.sec = math.floor(e % 60)
+    date.isdst = false
+    return date
 end
 
 --- Returns a table containing the sunet and runrise time for the given
@@ -95,26 +163,26 @@ end
 ---@param date osdate
 ---@return table
 local calc_sun_times = function(latitude, longitude, date)
-	local j = date_to_julian(date)
-	local mst = math.ceil(j - 2451545.0008) - (longitude / 360)
-	local msa = (357.5291 + (0.98560028 * mst)) % 360
-	local c = (1.9148 * sin(msa)) + (0.02 * sin(2 * msa)) + (0.0003 * sin(3 * msa))
-	local e = (msa + c + 282.9372) % 360
-	local st = 2451545 + mst + (0.0053 * sin(msa)) - (0.0069 * sin(2 * e))
-	local sd = asin(sin(e) * sin(23.44))
-	local h_cos = (sin(-0.83) - (sin(latitude) * sin(sd))) / (cos(latitude) * cos(sd))
+    local j = date_to_julian(date)
+    local mst = math.ceil(j - 2451545.0008) - (longitude / 360)
+    local msa = (357.5291 + (0.98560028 * mst)) % 360
+    local c = (1.9148 * sin(msa)) + (0.02 * sin(2 * msa)) + (0.0003 * sin(3 * msa))
+    local e = (msa + c + 282.9372) % 360
+    local st = 2451545 + mst + (0.0053 * sin(msa)) - (0.0069 * sin(2 * e))
+    local sd = asin(sin(e) * sin(23.44))
+    local h_cos = (sin(-0.83) - (sin(latitude) * sin(sd))) / (cos(latitude) * cos(sd))
 
-	-- sun never rises or sets
-	if h_cos > 1 or h_cos < -1 then
-		return { sunrise = -1, sunset = -1 }
-	end
+    -- sun never rises or sets
+    if h_cos > 1 or h_cos < -1 then
+        return { sunrise = -1, sunset = -1 }
+    end
 
-	local h = acos((sin(-0.83) - (sin(latitude) * sin(sd))) / (cos(latitude) * cos(sd))) / 360
+    local h = acos((sin(-0.83) - (sin(latitude) * sin(sd))) / (cos(latitude) * cos(sd))) / 360
 
-	return {
-		sunrise = os.time(julian_to_greg(st - h)),
-		sunset = os.time(julian_to_greg(st + h)),
-	}
+    return {
+        sunrise = os.time(julian_to_greg(st - h)),
+        sunset = os.time(julian_to_greg(st + h)),
+    }
 end
 
 --- Returns a table containing the next sunrise and sunset times. Offsets are
@@ -125,130 +193,33 @@ end
 ---@param sunset_offset number
 ---@return table
 M.next_sun_times = function(latitude, longitude, sunrise_offset, sunset_offset)
-	local date = os.date("!*t")
-	local time = os.time(date)
-	date.hour = 0
+    local date = os.date("!*t")
+    local time = os.time(date)
+    date.hour = 0
 
-	-- first get sunset and sunrise for the current date
-	local sun_times = calc_sun_times(latitude, longitude, date)
-	local sunrise = sun_times.sunrise + sunrise_offset
-	local sunset = sun_times.sunset + sunset_offset
+    -- first get sunset and sunrise for the current date
+    local sun_times = calc_sun_times(latitude, longitude, date)
+    local sunrise = sun_times.sunrise + sunrise_offset
+    local sunset = sun_times.sunset + sunset_offset
 
-	local i = 0
-	while sunrise < time or sunset < time do
-		i = i + 1
-		if i > 365 then
-			M.notify.warn("Unsupported latitude, longitude coordinates.")
-			return { sunrise = nil, sunset = nil }
-		end
-		date.day = date.day + 1
-		sun_times = calc_sun_times(latitude, longitude, date)
-		if sunrise < time then
-			sunrise = sun_times.sunrise + sunrise_offset
-		end
+    local i = 0
+    while sunrise < time or sunset < time do
+        i = i + 1
+        if i > 365 then
+            M.notify.error("Unsupported latitude, longitude coordinates.")
+            return { sunrise = nil, sunset = nil }
+        end
+        date.day = date.day + 1
+        sun_times = calc_sun_times(latitude, longitude, date)
+        if sunrise < time then
+            sunrise = sun_times.sunrise + sunrise_offset
+        end
 
-		if sunset < time then
-			sunset = sun_times.sunset + sunset_offset
-		end
-	end
-	return { sunrise = sunrise, sunset = sunset }
+        if sunset < time then
+            sunset = sun_times.sunset + sunset_offset
+        end
+    end
+    return { sunrise = sunrise, sunset = sunset }
 end
 
---- Notify with plugin title
----@param message string
----@param level number
-local notify_func = function(message, level)
-	vim.notify(message, level, { title = "sunset.nvim", timeout = 20000 })
-end
-
-M.notify = {
-	debug = function(message)
-		notify_func(message, vim.log.levels.DEBUG)
-	end,
-	error = function(message)
-		notify_func(message, vim.log.levels.ERROR)
-	end,
-	info = function(message)
-		notify_func(message, vim.log.levels.INFO)
-	end,
-	warn = function(message)
-		notify_func(message, vim.log.levels.WARN)
-	end,
-}
-
---- Converts a string representing a 24 hour time in the form HH:MM to the next
---- occurence of this time as a system datetime value. Returns nil if the
---- provided string is not a valid time.
----@param time_str string
----@return number|nil
-M.str_to_next_time = function(time_str)
-	if not time_str or time_str == "" then
-		return nil
-	end
-
-	-- parse string
-	local hour, min = string.match(time_str, "(%d?%d):(%d%d)")
-	if not hour or not min then
-		M.notify.error("Invalid time format provided. Supported format is HH:MM.")
-		return nil
-	elseif tonumber(hour) > 24 or tonumber(min) > 59 then
-		M.notify.error("Invalid time provided. Must be a 24-hour time.")
-		return nil
-	end
-
-	local date = os.date("!*t")
-	local current_time = os.time(date)
-	date.hour = tonumber(hour)
-	date.min = tonumber(min)
-	date.sec = 0
-
-	local next_time = os.time(date)
-	if next_time < current_time then
-		date.day = date.day + 1
-		next_time = os.time(date)
-	end
-	return next_time
-end
-
-M.set_background = function(background)
-	if api.nvim_get_option("background") ~= background then
-		api.nvim_set_option("background", background)
-	end
-end
-
--- local function test()
--- 	local date = os.date("!*t")
--- 	date.hour = 0
--- 	for i = 1, 1000000, 1 do
--- 		if i % 10000 == 0 then
--- 			print("Test " .. i)
--- 		end
--- 		-- generate random lat and long
--- 		local lat = math.random() + math.random(-90, 89)
--- 		local long = math.random() + math.random(-180, 179)
---
--- 		M.next_sun_times(lat, long, 0, 0)
--- 	end
--- end
-
--- test()
-
--- for i, coord in pairs(lats) do
--- 	print(i)
--- 	local next_sun_times = M.next_sun_times(coord[1], coord[2], 0, 0)
--- 	print(
--- 		"Next sunrise is at",
--- 		os.date("!%c", next_sun_times.sunrise),
--- 		"sunset is at",
--- 		os.date("!%c", next_sun_times.sunset)
--- 	)
--- end
-
--- local next_sun_times = M.next_sun_times(90, -135, 0, 0)
--- print(
--- 	"Next sunrise is at",
--- 	os.date("!%c", next_sun_times.sunrise),
--- 	"sunset is at",
--- 	os.date("!%c", next_sun_times.sunset)
--- )
 return M
